d0a317bd0a49fc3d4d8c812fc1fff87d001e8e49
eunjeong <eunjeong990731@gmail.com>
2023-11-28 10:39:57 +0900

    :adhesive_bandage: Resolve:
    dirname을 쓴다면 path 설정을 해줘야한다.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require(express);
-const app = express;
+const express = require('express');
+const app = express();
 const port = 3000;
+const path = require('path');
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '');
+  res.sendFile(path.join(__dirname, 'front', 'index.html'));
 });
 
 app.listen(port, () => {
